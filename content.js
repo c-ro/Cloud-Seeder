fba828145b4cd392b2c0e96d1ad2afcf8d31e3ec
c-ro <carlryanphoto@gmail.com>
2015-08-11 14:11:41 -0400

    CS can now auto-download all visible/dlable files. uses [].slice.call to enable .click() on the button

--- content.js
+++ content.js
@@ -4,26 +4,35 @@ $(document).ready(function() {
 	$(".l-three-column").prepend("<div id='dlNotice'>Downloads: </div>");
 	$('#dlNotice').hide();
 	
-	var dlButtons = $(".sc-button-download.sc-button.sc-button-small.sc-button-responsive");
-	// <a class="sc-button-download sc-button sc-button-small sc-button-responsive" tabindex="0" download="Pizza Mix 018 - Crush All Bits" title="Download this track (40.8MB)">Download</a>
+	// var dlButtons = $(".sc-button-download.sc-button.sc-button-small.sc-button-responsive");
 
 	var dlLabels = [];
 
-	$(".sc-button-download.sc-button.sc-button-small.sc-button-responsive").each(function () {
-	  dlLabels.push($(this).attr("download"));
-	});
 
 	// TODO: Capture/draw each available button indiviually
 	// TODO: eager load the whole profile to get all buttons.
 	chrome.runtime.onMessage.addListener(
 	  function(request, sender, sendResponse) {
 	    if(request.message === "clicked_page_action") {
+
+
+			$(".sc-button-download").each(function () {
+				var arrayLike = [].slice.call(arguments);
+				arrayLike[1].click();
+				console.log(arrayLike[1]);
+
+				// <span class="soundTitle__usernameText">pixelord</span>
+
+				// console.log(this);
+				// arrayLike[1].click();//.ownerDocument.activeElement.href
+			});
+
 	    		//remove dl button from view and scrape data
-	    		dlButtons.detach();
+	    		// dlButtons.detach();
 	    		// redraw dl button in dlNotice div with text identifier
-	    		$('#dlNotice').toggle();
+	    		// $('#dlNotice').toggle();
 	    		// $('#dlNotice').append($(dlButtons).attr("download"));  <--- this works for first element only
-	    		$(dlButtons).appendTo('#dlNotice');
+	    		// $(dlButtons).appendTo('#dlNotice');
 
 		};
  	});
